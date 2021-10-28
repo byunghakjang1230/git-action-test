0f97970a22cb8d5711e27127fa3d7d835a80d0e6
byunghakjang1230 <byunghakjang1230@gmail.com>
2021-10-28 23:19:34 +0900

    first commit

---
+++ src/main/java/kr/co/kumsung/gitactiontest/GitActionTestApplication.java
@@ -0,0 +1,13 @@
+package kr.co.kumsung.gitactiontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitActionTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitActionTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/kumsung/gitactiontest/GitActionTestApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.kumsung.gitactiontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitActionTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
